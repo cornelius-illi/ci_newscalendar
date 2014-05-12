70a2352ae62edaf15df8234e8a9d9d60df7689a3
Cornelius Illi <cornelius.illi@hintertuer.net>
2014-05-12 10:51:55 +0200

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "newscalendar".
+ *
+ * Auto generated 22-02-2013 20:48
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'News Calendar',
+	'description' => 'Calendar for Ext:tt_news. Provides calendar and a list view functionality. Main sponsors: geefgratis.nl, eventonizer.nl, phase2-networks.com, digital-park.de',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'tt_news',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Cornelius Illi, Philip Almeida, Clemens Riccabona',
+	'author_email' => 'mail@corneliusilli.de, philip.almeida@freedomson.com, clemens@riccabona.biz',
+	'author_company' => 'freedomson.com eur-ops.com',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
